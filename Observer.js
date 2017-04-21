3813e017cf159798dd5ecc9e806bcf60458b9f5f
Stephen Poole <stephen.poole@redlioncanada.com>
2017-04-21 10:55:07 -0400

    Removed let and implicit object assignment to fully support ES5

--- Observer.js
+++ Observer.js
@@ -1,31 +1,39 @@
 var Observer = function() {
-	let subjects = {};
-	return {
-		on: function(e, fn, args) {
-			if (typeof e !== 'string' || !e || typeof fn !== 'function') return;
-			if (!(e in subjects)) subjects[e] = [];
-			subjects[e].push({event: e, fn, args});
-		},
-		emit: function(e, args) {
-			if (e in subjects) {
-				for (var index in subjects[e]) {
-					let subject = subjects[e][index];
-					subject.fn.call(this, subject.args, args);
-				}
-			}
-		},
-		off: function(e, fn) {
-			if (!(e in subjects)) return;
-			if (typeof fn !== 'undefined' && typeof fn === 'function') {
-				for (var index in subjects[e]) {
-					let subject = subjects[e][index];
-					if (subject.fn === fn) subjects[e].splice(0, index);
-				}
-			} else {
-				delete subjects[e];
-			}
-		}
-	}
+    var subjects = {};
+    return {
+        on: function(e, fn, args) {
+            if (typeof e !== 'string' || !e || typeof fn !== 'function') {
+                return;
+            }
+            if (!(e in subjects)) {
+                subjects[e] = [];
+            }
+            subjects[e].push({ event: e, fn: fn, args: args });
+        },
+        emit: function(e, args) {
+            if (e in subjects) {
+                for (var index in subjects[e]) {
+                    var subject = subjects[e][index];
+                    subject.fn.call(this, subject.args, args);
+                }
+            }
+        },
+        off: function(e, fn) {
+            if (!(e in subjects)) {
+                return;
+            }
+            if (typeof fn === 'function') {
+                for (var index in subjects[e]) {
+                    var subject = subjects[e][index];
+                    if (subject.fn === fn) {
+                        subjects[e].splice(0, index);
+                    }
+                }
+            } else {
+                delete subjects[e];
+            }
+        }
+    }
 };
 
 module.exports = Observer
